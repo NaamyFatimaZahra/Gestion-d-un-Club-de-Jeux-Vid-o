7b1059b4ae4d1dc31ea4a3506e5adcb0e46b7bbe
naamyfatimazahra <naamyfatimazahra@gmail.com>
2024-12-11 10:09:22 +0100

    finish mld

---
+++ db.sql
@@ -0,0 +1,12 @@
+--  La création du DB (DataBase) 
+
+     CREATE DataBase gestion_club_jeux;
+
+--  La création du tableau  
+
+     CREATE DataBase gestion_club_jeux    
+
+--  La création du tableau  
+
+     USE gestion_club_jeux
+
